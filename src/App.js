a5b06734857b8f94f1dbc9a4de9616cedd66023e
oumar <oumardialo98@qq.com>
2022-01-13 13:11:11 +0800

    e-commerce

--- src/App.js
+++ src/App.js
@@ -1,11 +1,106 @@
+import React, { useState, useEffect } from 'react';
+import { CssBaseline } from '@material-ui/core';
+import { Route,Routes } from 'react-router-dom';
 
-function App()
+import { Navbar, Products, Cart, Checkout } from './components';
+import { commerce } from './lib/commerce';
+
+const App = () =>
 {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const fetchProducts = async () =>
+  {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  const fetchCart = async () =>
+  {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quantity) =>
+  {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  };
+
+  const handleUpdateCartQty = async (lineItemId, quantity) =>
+  {
+    const response = await commerce.cart.update(lineItemId,  { quantity });
+
+    setCart(response.cart);
+  };
+
+  const handleRemoveFromCart = async (lineItemId) =>
+  {
+    const response = await commerce.cart.remove(lineItemId);
+
+    setCart(response.cart);
+  };
+
+  const handleEmptyCart = async () =>
+  {
+    const response = await commerce.cart.empty();
+
+    setCart(response.cart);
+  };
+
+  const refreshCart = async () =>
+  {
+    const newCart = await commerce.cart.refresh();
+
+    setCart(newCart);
+  };
+
+  const handleCaptureCheckout = async (checkoutTokenId, newOrder) =>
+  {
+    try
+    {
+      const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+
+      setOrder(incomingOrder);
+
+      await refreshCart();
+    } catch (error)
+    {
+      setErrorMessage(error.data.error.message);
+    }
+  };
+
+  useEffect(() =>
+  {
+    fetchProducts().then(() =>{} );
+    fetchCart().then(() =>{} );
+  }, []);
+
+  const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
+
   return (
-    <div className="App">
-        Bonjour
-    </div>
+
+      <div style={{ display: 'flex' }}>
+        <CssBaseline />
+        <Navbar totalItems={cart.total_items} handleDrawerToggle={handleDrawerToggle} />
+        <Routes>
+          <Route exact path="/"
+           element={ <Products products={products} onAddToCart={handleAddToCart} handleUpdateCartQty />}
+          />
+          <Route exact path="/cart"
+            element={<Cart cart={cart} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} onEmptyCart={handleEmptyCart} />}
+          />
+          <Route path="/checkout" exact
+            element={<Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage} />}
+          />
+        </Routes>
+      </div>
   );
-}
+};
 
 export default App;
